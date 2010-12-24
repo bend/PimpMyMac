e60a6bba018adf4b7aeb6ad98e49a64821541772
Benoit Daccache <ben.daccache@gmail.com>
2010-12-24 08:35:45 +0100

    first import

---
+++ PimpMyMac.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-12-24T08:21:33
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = PimpMyMac
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    ui.cpp \
+    commandexecuter.cpp
+
+HEADERS  += \
+    ui.h \
+    commandexecuter.h
+
+FORMS    +=
---
+++ ui.h
@@ -0,0 +1,43 @@
+/*
+*   Copyright © Benoit Daccache
+*   ben.daccache@gmail.com
+*
+*
+*   This is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef UI_H
+#define UI_H
+
+#include <QMainWindow>
+#include <QLayout>
+#include <QPushButton>
+#include <QRadioButton>
+
+class UI : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit UI(QWidget *parent = 0);
+
+private:
+    void setupUI();
+
+signals:
+
+public slots:
+
+};
+
+#endif // UI_H
